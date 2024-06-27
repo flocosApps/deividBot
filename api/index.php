c2dee9d2bd57baa597c5dfd19d43e0b1ad97f16f
flocosApps <138632394+flocosApps@users.noreply.github.com>
2024-06-27 15:42:08 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,14 +3,12 @@
 $botToken = '7379108354:AAFEe7_zPtrtvvX3iETSnk6T6U48Yf0Cxk8';
 $apiUrl = "https://api.telegram.org/bot$botToken/";
 
-// Função para enviar mensagem de resposta ao usuário
 function sendMessage($chatId, $message) {
     global $apiUrl;
     $url = $apiUrl . "sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message);
     file_get_contents($url);
 }
 
-// Captura a entrada JSON do webhook
 $update = file_get_contents("php://input");
 $updateArray = json_decode($update, true);
 
@@ -21,12 +19,10 @@ if (isset($updateArray['message'])) {
 
     sendMessage($chatId, "OLA");
 
-    // Verifica se a mensagem contém o número de série
     if (!empty($text)) {
         $sn = $text;
         $url = 'https://script.google.com/macros/s/AKfycbwVL_A2dJyq04tlasJk5-joNf2j22FhoDyHjD_XpBTh5F2FypkLpTfiwf7Q33Mb5b0arQ/exec';
 
-        // Parâmetros da requisição
         $params = [
             'page' => 'test',
             'columnID' => 'SN',
@@ -36,7 +32,6 @@ if (isset($updateArray['message'])) {
             'url' => 'https://docs.google.com/spreadsheets/d/1tsw-O6LJ-NC3nQ4rlrQcpUUnRGBG6M3dkYQ6XPK4IXg/edit?'
         ];
 
-        // Configurações da requisição POST
         $options = [
             'http' => [
                 'header'  => "Content-type: application/json\r\n",
@@ -45,7 +40,6 @@ if (isset($updateArray['message'])) {
             ],
         ];
 
-        // Cria o contexto da requisição
         $context  = stream_context_create($options);
 
         // Faz a requisição POST e obtém a resposta
@@ -56,10 +50,7 @@ if (isset($updateArray['message'])) {
             exit;
         }
 
-        // Decodifica a resposta JSON
         $response = json_decode($result, true);
-
-        // Verifica se a resposta contém o status 200 e a senha
         sendMessage($chatId, $result);
         if (isset($response['status']) && $response['status'] === 200 && isset($response['password'])) {
             sendMessage($chatId, "Password: " . $response['password']);
